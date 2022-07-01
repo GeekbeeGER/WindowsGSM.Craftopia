5c654089ea6249ceddac9ef1c6cffb5361812611
Geekbee <39613357+GeekbeeGER@users.noreply.github.com>
2022-07-01 20:31:32 +0200

    Add files via upload

---
+++ Craftopia.cs/Craftopia.cs/Craftopia.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+
+namespace WindowsGSM.Plugins
+{
+    public class Craftopia : SteamCMDAgent // SteamCMDAgent is used because Craftopia relies on SteamCMD for installation and update process
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.Craftopia", // WindowsGSM.XXXX
+            author = "GeekbeeGER",
+            description = "WindowsGSM plugin for supporting Craftopia Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/GeekbeeGER/WindowsGSM.Craftopia", // Github repository link (Best practice)
+            color = "#9eff99" // Color Hex
+        };
+
+
+        // - Standard Constructor and properties
+        public Craftopia(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData; // Store server start metadata, such as start ip, port, start param, etc
+
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true; // Craftopia requires to login steam account to install the server, so loginAnonymous = false
+        public override string AppId => "1670340"; // Game server appId, Craftopia is 233780
+
+
+        // - Game server Fixed variables
+        public override string StartPath => "Craftopia.exe"; // Game server start path, for Craftopia, it is Craftopiaserver.exe
+        public string FullName = "Craftopia"; // Game server FullName
+        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
+        public int PortIncrements = 2; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+
+        // - Game server default values
+        public string Port = "6587"; // Default port
+        public string QueryPort = "4380"; // Default query port
+        public string Defaultmap = "empty"; // Default map name
+        public string Maxplayers = "64"; // Default maxplayers
+        public string Additional = " -batchmode -showlogs"; // Additional server start parameter
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG() { }
+
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            // Prepare start parameter
+            var param = new StringBuilder();
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -port={_serverData.ServerPort}");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerName) ? string.Empty : $" -name=\"{_serverData.ServerName}\"");
+            param.Append(string.IsNullOrWhiteSpace(_serverData.ServerParam) ? string.Empty : $" {_serverData.ServerParam}");
+ 
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false,
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath),
+                    Arguments = param.ToString()
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Start Process
+            try
+            {
+                p.Start();
+                return p;
+            }
+            catch (Exception e)
+            {
+                base.Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+
+        // - Stop server function
+        public async Task Stop(Process p) => await Task.Run(() => { p.Kill(); }); // I believe Craftopia don't have a proper way to stop the server so just kill it
+    }
+}
